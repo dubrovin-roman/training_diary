64d3393da5e09ea8aefa4a72d1b3509cdf56d3ee
Roman <dubrovin.skfo@gmail.com>
2023-06-11 17:34:47 +0300

    подключил Yandex API Карты

--- script.js
+++ script.js
@@ -22,3 +22,36 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords;
+      const myCoordinates = [latitude, longitude];
+      ymaps.ready(init);
+      function init() {
+        // Создание карты.
+        const myMap = new ymaps.Map("map", {
+          // Координаты центра карты.
+          // Порядок по умолчанию: «широта, долгота».
+          // Чтобы не определять координаты центра карты вручную,
+          // воспользуйтесь инструментом Определение координат.
+          center: myCoordinates,
+          // Уровень масштабирования. Допустимые значения:
+          // от 0 (весь мир) до 19.
+          zoom: 13,
+        });
+        const myPosition = new ymaps.GeoObject({
+          geometry: {
+            type: "Point", // тип геометрии - точка
+            coordinates: myCoordinates, // координаты точки
+          },
+        });
+        myMap.geoObjects.add(myPosition);
+      }
+    },
+    function () {
+      alert("Вы не предоставили свою геопозицию");
+    }
+  );
+}
