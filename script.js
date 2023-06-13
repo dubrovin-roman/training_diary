aa88bafbb12fd900046d2786bc6b173c12f8c098
Roman <dubrovin.skfo@gmail.com>
2023-06-13 20:41:57 +0300

    создал класс TrainingDiaryApp, перенес код туда

--- script.js
+++ script.js
@@ -23,6 +23,77 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+class TrainingDiaryApp {
+  _myMap;
+  _mapEvent;
+
+  constructor() {
+    inputType.addEventListener(
+      "change",
+      this._changingFormWhenSwitchingTypeOfTraining
+    );
+    form.addEventListener("submit", this._submittingForm.bind(this));
+  }
+
+  initApp() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), () =>
+        alert("Вы не предоставили свою геопозицию")
+      );
+    }
+  }
+
+  _loadMap(position) {
+    const { latitude, longitude } = position.coords;
+    const myCoordinates = [latitude, longitude];
+    ymaps.ready(init.bind(this));
+    function init() {
+      this._myMap = new ymaps.Map("map", {
+        center: myCoordinates,
+        zoom: 13,
+      });
+      this._myMap.events.add("click", this._showForm.bind(this));
+    }
+  }
+
+  _showForm(ev) {
+    this._mapEvent = ev;
+    form.classList.remove("hidden");
+    inputDistance.focus();
+  }
+
+  _submittingForm(ev) {
+    ev.preventDefault();
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        "";
+    inputDistance.focus();
+    const coords = this._mapEvent.get("coords");
+    const tempBalloon = new ymaps.Placemark(
+      coords,
+      {
+        iconContent: "Тренировка № 1",
+        balloonContentHeader: "Тренировка № 1",
+        balloonContentBody: "Содержимое тренировки",
+      },
+      {
+        preset: "islands#darkGreenStretchyIcon",
+      }
+    );
+    this._myMap.geoObjects.add(tempBalloon);
+  }
+
+  _changingFormWhenSwitchingTypeOfTraining() {
+    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+  }
+}
+
+const app = new TrainingDiaryApp();
+app.initApp();
+/*
 let myMap;
 let mapEvent;
 
@@ -44,32 +115,10 @@ if (navigator.geolocation) {
           // от 0 (весь мир) до 19.
           zoom: 13,
         });
-        const myPosition = new ymaps.GeoObject({
-          geometry: {
-            type: "Point", // тип геометрии - точка
-            coordinates: myCoordinates, // координаты точки
-          },
-        });
-        myMap.geoObjects.add(myPosition);
         myMap.events.add("click", (ev) => {
           mapEvent = ev;
           form.classList.remove("hidden");
           inputDistance.focus();
-          /*
-          const coords = mapEvent.get("coords");
-          const tempBalloon = new ymaps.Placemark(
-            coords,
-            {
-              iconContent: "Тренировка № 1",
-              balloonContentHeader: "Тренировка № 1",
-              balloonContentBody: "Содержимое тренировки",
-            },
-            {
-              preset: "islands#darkGreenStretchyIcon",
-            }
-          );
-          myMap.geoObjects.add(tempBalloon);
-          */
         });
       }
     },
@@ -104,3 +153,4 @@ if (navigator.geolocation) {
     inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
   });
 }
+*/
