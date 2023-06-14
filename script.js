2d65bcea0f9112dc67aa8b3a1289ced31cadf84a
Roman Dubrovin <dubrovin.skfo@gmail.com>
2023-06-14 17:30:06 +0300

    валидация форм, создание объектов, общий массив
    добавил валидацию форм, создание объектов добавление тренировок в массив

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
 class TrainingDiaryApp {
+  _workouts = [];
   _myMap;
   _mapEvent;
 
@@ -64,17 +65,64 @@ class TrainingDiaryApp {
     inputDistance.focus();
   }
 
+  _hiddenForm() {
+    form.classList.add("hidden");
+  }
+
   _submittingForm(ev) {
     ev.preventDefault();
+    //получаем координаты из mapEvent
+    const coords = this._mapEvent.get("coords");
+    //получаем данные из форм и валидируем их
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
+    let workout;
+
+    //функции для валидации
+    const isAllPositive = (...inputs) => inputs.every((value) => value > 0);
+    const isAllNumber = (...inputs) =>
+      inputs.every((value) => Number.isFinite(value));
+
+    if (type === "running") {
+      const cadence = +inputCadence.value;
+      if (
+        !isAllNumber(distance, duration, cadence) ||
+        !isAllPositive(distance, duration, cadence)
+      ) {
+        return alert("Необходимо ввести целое положительное число");
+      }
+      workout = new Running(coords, distance, duration, cadence);
+    }
+
+    if (type === "cycling") {
+      const elevation = +inputElevation.value;
+      if (
+        !isAllNumber(distance, duration, elevation) ||
+        !isAllPositive(distance, duration)
+      ) {
+        return alert("Необходимо ввести целое положительное число");
+      }
+      workout = new Cycling(coords, distance, duration, elevation);
+    }
+    //складываем все тренировки в один массив
+    this._workouts.push(workout);
+    //отрисовка тренировки на карте
+    this._renderingWorkoutOnMap(workout);
+
     inputDistance.value =
       inputDuration.value =
       inputCadence.value =
       inputElevation.value =
         "";
-    inputDistance.focus();
-    const coords = this._mapEvent.get("coords");
+
+    //скрываем форму ввода
+    this._hiddenForm();
+  }
+
+  _renderingWorkoutOnMap(workout) {
     const tempBalloon = new ymaps.Placemark(
-      coords,
+      workout.coord,
       {
         iconContent: "Тренировка № 1",
         balloonContentHeader: "Тренировка № 1",
